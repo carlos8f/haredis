7405ea3fde58574e843d5df7bf967e4389346159
Carlos Rodriguez <carlos@s8f.org>
2012-07-06 13:15:24 -0700

    Add "haredis" log prefix

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ RedisHAClient.prototype.error = function(message, label) {
   }
 };
 RedisHAClient.prototype.logFormat = function(type, message) {
-  return util.format('[%s](#%d) %s: %s', new Date().toTimeString().split(' ')[0], this.connection_id, type, message);
+  return util.format('[%s](haredis#%d) %s: %s', new Date().toTimeString().split(' ')[0], this.connection_id, type, message);
 };
 
 RedisHAClient.prototype.onReady = function() {
