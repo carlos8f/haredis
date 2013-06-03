e8b89f35853781ec9c92ed391d9b39421b83bc16
Carlos Rodriguez <carlos@s8f.org>
2013-06-03 14:03:39 -0700

    abstract common test stuff

--- test/_common.js
+++ test/_common.js
@@ -5,3 +5,93 @@ spawn = require('child_process').spawn;
 rimraf = require('rimraf');
 mkdirp = require('mkdirp');
 idgen = require('idgen');
+
+servers = {};
+ports = [6380, 6381, 6382];
+testId = idgen(4);
+
+makeServer = function (port, cb) {
+  var dir = '/tmp/haredis-test-' + testId + '/' + port;
+  mkdirp(dir, function (err) {
+    if (err) return cb(err);
+    var child = spawn('redis-server', ['--port', port, '--dir', dir, '--slave-read-only', 'no']);
+    var started = false;
+    child.stdout.on('data', function (chunk) {
+      if (~String(chunk).indexOf('The server is now ready')) {
+        started = true;
+        cb(null, child);
+      }
+    });
+    child.stderr.pipe(process.stderr);
+    setTimeout(function () {
+      if (!started) throw new Error('redis-server on port ' + port + ' failed to start');
+    }, 10000);
+  });
+};
+
+makeServers = function (cb) {
+  var tasks = {};
+  ports.forEach(function (port) {
+    tasks[port] = makeServer.bind(null, port);
+  });
+
+  async.parallel(tasks, function (err, results) {
+    assert.ifError(err);
+    servers = results;
+    cb();
+  });
+};
+
+process.on('exit', function () {
+  Object.keys(servers).forEach(function (port) { servers[port].kill(); });
+  rimraf.sync('/tmp/haredis-test-' + testId);
+});
+
+createClient = function () {
+  var client = haredis.createClient(ports);
+  // redirect logging
+  client.warn = client.error = client.log = log;
+
+  client.tail = '';
+
+  function log () {
+    var args = [].slice.call(arguments);
+    var line = args.join(' ') + '\n';
+    client.tail += line;
+    //process.stdout.write(line);
+  };
+
+  client.waitFor = function (pattern, cb) {
+    if (Array.isArray(pattern)) {
+      var list = pattern.slice();
+      var latch = list.length;
+      (function doNext () {
+        var pat = list.shift();
+        client.waitFor(pat, function () {
+          if (!--latch) cb();
+          else doNext();
+        });
+      })();
+      return;
+    }
+    var timeout, found = false;
+    (function search () {
+      if (found) return;
+      client.tail.split('\n').forEach(function (line, idx) {
+        if (found) return;
+        if (line.match(pattern)) {
+          found = true;
+          client.tail = client.tail.split('\n').slice(idx).join('\n');
+          cb();
+        }
+      });
+      if (!found) timeout = setTimeout(search, 500);
+    })();
+    setTimeout(function () {
+      clearTimeout(timeout);
+      if (!found) throw new Error('timed out waiting for `' + pattern + '`');
+    }, 30000);
+  };
+
+  return client;
+};
--- test/failover.js
+++ test/failover.js
@@ -1,90 +1,9 @@
 describe('failover', function () {
-  var servers
-    , ports = [6380, 6381, 6382]
-    , testId = idgen(4)
-    , tail = ''
-
-  function log () {
-    var args = [].slice.call(arguments);
-    var line = args.join(' ') + '\n';
-    tail += line;
-    //process.stdout.write(line);
-  };
-
-  function waitFor (pattern, cb) {
-    if (Array.isArray(pattern)) {
-      var list = pattern.slice();
-      var latch = list.length;
-      (function doNext () {
-        var pat = list.shift();
-        waitFor(pat, function () {
-          if (!--latch) cb();
-          else doNext();
-        });
-      })();
-      return;
-    }
-    var timeout, found = false;
-    (function search () {
-      if (found) return;
-      tail.split('\n').forEach(function (line, idx) {
-        if (found) return;
-        if (line.match(pattern)) {
-          found = true;
-          tail = tail.split('\n').slice(idx).join('\n');
-          cb();
-        }
-      });
-      if (!found) timeout = setTimeout(search, 500);
-    })();
-    setTimeout(function () {
-      clearTimeout(timeout);
-      if (!found) throw new Error('timed out waiting for `' + pattern + '`');
-    }, 30000);
-  }
-
-  function makeServer (port, cb) {
-    var dir = '/tmp/haredis-test-' + testId + '/' + port;
-    mkdirp(dir, function (err) {
-      if (err) return cb(err);
-      var child = spawn('redis-server', ['--port', port, '--dir', dir, '--slave-read-only', 'no']);
-      var started = false;
-      child.stdout.on('data', function (chunk) {
-        if (~String(chunk).indexOf('The server is now ready')) {
-          started = true;
-          cb(null, child);
-        }
-      });
-      child.stderr.pipe(process.stderr);
-      setTimeout(function () {
-        if (!started) throw new Error('redis-server on port ' + port + ' failed to start');
-      }, 10000);
-    });
-  }
-
-  process.on('exit', function () {
-    if (servers) Object.keys(servers).forEach(function (port) { servers[port].kill(); });
-    rimraf.sync('/tmp/haredis-test-' + testId);
-  });
-
-  before(function (cb) {
-    var tasks = {};
-    ports.forEach(function (port) {
-      tasks[port] = makeServer.bind(null, port);
-    });
-
-    async.parallel(tasks, function (err, results) {
-      assert.ifError(err);
-      servers = results;
-      cb();
-    });
-  });
+  before(makeServers);
 
   var client, masterPort, newMasterPort;
   it('create client', function (done) {
-    client = haredis.createClient(ports);
-    // redirect logging
-    client.warn = client.error = client.log = log;
+    client = createClient();
 
     var latch = 2;
     client.once('connect', function () {
@@ -92,7 +11,7 @@ describe('failover', function () {
       if (!--latch) done();
     });
 
-    waitFor([
+    client.waitFor([
       'invalid master count: 3',
       'elected .* as master!',
       'making .* into a slave...',
@@ -119,7 +38,7 @@ describe('failover', function () {
   });
 
   it('kill master', function (done) {
-    waitFor([
+    client.waitFor([
       'MASTER connection dropped, reconnecting...',
       'Error: .* connection dropped and reconnection failed!',
       'MASTER is down!',
@@ -159,7 +78,7 @@ describe('failover', function () {
   });
 
   it('restart old master', function (done) {
-    waitFor('reorientating \\(evaluating 127.0.0.1:' + masterPort + '\\) in 2000ms', done);
+    client.waitFor('reorientating \\(evaluating 127.0.0.1:' + masterPort + '\\) in 2000ms', done);
     makeServer(masterPort, function (err, server) {
       assert.ifError(err);
       servers[masterPort] = server;
@@ -168,7 +87,7 @@ describe('failover', function () {
 
   it('old master is made into slave', function (done) {
     // @todo: waitFor message that masterPort made into slave
-    waitFor([
+    client.waitFor([
       'invalid master count: 2',
       'attempting failover!',
       'elected .* as master!',
